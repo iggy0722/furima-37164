73eaa4b877fca7742e6843e5eacddbe6b136df2b
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-11-24 14:34:45 +0900

    user新規登録画面ビューファイル編集　rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_day, presence: true
-
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
