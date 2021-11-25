c1ec2dfbe6a33d929a7964cb4078cfddd84d4320
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-11-25 18:30:56 +0900

    modelにバリデーション、各アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :items
+
+
   validates :nickname, presence: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
