76281542d109efb0e53b25d4ed87f1b220ef3ae9
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-11-25 20:26:28 +0900

    ストロングパラメーター、モデル内変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
 
+  
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true
@@ -11,4 +13,5 @@ class Item < ApplicationRecord
   validates :delivery_day_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
+  validates :image, presence: true
 end
