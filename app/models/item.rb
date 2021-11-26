3b89dfe2f184e11d97cc8c6fda22cdb07f12cb3e
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-11-26 11:07:55 +0900

    各アクティブハッシュ配置

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
+  
   belongs_to :user
   has_one_attached :image
 
