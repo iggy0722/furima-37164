ad97a561ac78642679c782ba2a0f2c8cfcae480a
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-11-26 18:17:16 +0900

    テストコード、java実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,13 @@ class Item < ApplicationRecord
   
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
   validates :price, presence: true, inclusion: { in: 300..9999999 }
   validates :user, presence: true
   validates :image, presence: true
+
 end
