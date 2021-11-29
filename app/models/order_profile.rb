d0e5fc8d49ff27e2245da688e29072950021f034
iggy0722 <iggy.mustbenice.0722@gmail.com>
2021-11-29 17:09:05 +0900

    バリデーション追記

--- app/models/order_profile.rb
+++ app/models/order_profile.rb
@@ -3,10 +3,10 @@ class OrderProfile
   attr_accessor :postal, :prefecture_id, :municipality, :address, :building, :phone_number, :item_id, :user_id
 
   with_options presence: true do
-    validates :postal
+    validates :postal, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number, numericality: {only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid'}
     validates :user_id
     validates :item_id
   end
